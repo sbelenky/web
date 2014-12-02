c0a0964a530a25fdcf34bfa152a104d43f81db83
sbelenky <vbelenky@yahoo.com>
2014-12-02 16:46:54 -0500

    Put some RWD.

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Slava Belenky</title>
+	<link href="css/screen.css" rel="stylesheet" />
+</head>
+<body>
+	<header>
+		<ul class="menu-full">
+			<li>Menu 1</li>
+			<li>Menu Long 2</li>
+			<li>Menu 3</li>
+		</ul>
+		<span class="menu-icon">DROPDOWN</span>
+	</header>
+	<div id="main">
+
+	</div>
+
+	<ul class="jsFloatingMenu floating-menu">
+		<li>Menu 1</li>
+		<li>Menu Long 2</li>
+		<li>Menu 3</li>
+	</ul>
+	
+</body>
+</html>
